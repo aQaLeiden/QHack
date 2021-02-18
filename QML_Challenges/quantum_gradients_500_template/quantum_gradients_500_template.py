d57446958e5a4982c5f7d5d68a697e842fae3827
askolik <andrea.skolik@gmail.com>
2021-02-18 09:28:58 +0100

    gradients_500

--- QML_Challenges/quantum_gradients_500_template/quantum_gradients_500_template.py
+++ QML_Challenges/quantum_gradients_500_template/quantum_gradients_500_template.py
@@ -33,6 +33,61 @@ def natural_gradient(params):
 
     # QHACK #
 
+    qnode(params)
+    unshifted = dev.state
+
+    def metric_tensor_entry(i, j, unshifted, shift=np.pi/2):
+        shifted_plus = params.copy()
+        shifted_plus[i] += shift
+        shifted_plus[j] += shift
+        qnode(shifted_plus)
+        forward = dev.state
+        ov_forward = abs(np.vdot(unshifted, forward))**2
+
+        shifted_min_plu = params.copy()
+        shifted_min_plu[i] -= shift
+        shifted_min_plu[j] += shift
+        qnode(shifted_min_plu)
+        min_plu = dev.state
+        ov_min_plu = abs(np.vdot(unshifted, min_plu)) ** 2
+
+        shifted_plu_min = params.copy()
+        shifted_plu_min[i] += shift
+        shifted_plu_min[j] -= shift
+        qnode(shifted_plu_min)
+        plu_min = dev.state
+        ov_plu_min = abs(np.vdot(unshifted, plu_min)) ** 2
+
+        shifted_minus = params.copy()
+        shifted_minus[i] -= shift
+        shifted_minus[j] -= shift
+        qnode(shifted_minus)
+        backward = dev.state
+        ov_backward = abs(np.vdot(unshifted, backward)) ** 2
+
+        me = (1/8) * (-ov_forward + ov_plu_min + ov_min_plu - ov_backward)
+
+        return me
+
+    def apply_parameter_shift(qnode, params, i):
+        shifted = params.copy()
+        shifted[i] += np.pi / 2
+        forward = qnode(shifted)  # forward evaluation
+
+        shifted[i] -= np.pi
+        backward = qnode(shifted)  # backward evaluation
+
+        return 0.5 * (forward - backward)
+
+    gradient = np.zeros([6], dtype=np.float64)
+    metric_tensor = np.zeros([6, 6], dtype=np.float64)
+    for i in range(6):
+        gradient[i] = apply_parameter_shift(qnode, params, i)
+        for j in range(6):
+            metric_tensor[i][j] = metric_tensor_entry(i, j, unshifted)
+
+    me_inv = np.linalg.inv(metric_tensor)
+    natural_grad = me_inv @ gradient
     # QHACK #
 
     return natural_grad
@@ -97,3 +152,4 @@ if __name__ == "__main__":
     updated_params = natural_gradient(params)
 
     print(*updated_params, sep=",")
+
